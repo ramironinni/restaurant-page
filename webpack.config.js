5bd414b5abffcd260d5ca03f67b9f2d9b500fa9f
ramironinni <ramironinni@gmail.com>
2021-07-06 13:57:11 -0300

    Install and congif webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -23,8 +24,18 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "src/index.html",
+            templateParameters: {
+                title: "Tres Empanadas Restaurant",
+            },
+        }),
+    ],
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
 };
